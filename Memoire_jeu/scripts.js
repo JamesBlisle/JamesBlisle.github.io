467cf6e68008241d5a61013c6aac18bbf8cad399
unknown <2352100@carrefour.cegepvicto.ca>
2023-12-04 13:19:07 -0500

    Version avec option(beta) ajoutée

--- Memoire_jeu/scripts.js
+++ Memoire_jeu/scripts.js
@@ -1,24 +1,29 @@
 const cards = document.querySelectorAll('.memory-card');
-
+var compte = 0;
+var comptEE= 0;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+
+
+let mscore = document.getElementById("Score");
+let essais = document.getElementById("EScore")
 
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
 
   this.classList.add('flip');
-
+//Retourne la carte quand tu clique dessus grâce à la classe
   if (!hasFlippedCard) {
     // first click
-    hasFlippedCard = true;
+    hasFlippedCard = true; //S'il a retourné la carte, l'info de la première carte est assinée à la carte choisie
     firstCard = this;
 
     return;
   }
 
-  // second click
+  // Après le deuxième clic
   secondCard = this;
 
   checkForMatch();
@@ -26,26 +31,41 @@ function flipCard() {
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-  isMatch ? disableCards() : unflipCards();
+  isMatch ? disableCards() : unflipCards(); //if (égale à l'autre), paralyse les cartes :(sinon), retourne les cartes pour voir leur dos
+  if(isMatch == true) {
+    compte++; //Si ce sont les mêmes cartes, le compte de paires est augmenté et on change le score pour ajouter le compte
+    let containscore = document.getElementById("meilleurScore");
+    let paires = document.getElementById("h1p");
+    paires.innerHTML = compte + " / 6";
+    if (compte == 6) {
+      let rotation = [{transform:"rotate(3600deg)"}];
+      paires.animate(rotation, 5000);
+    }
+  }
+  else {
+    comptEE++;
+    let containessais = document.getElementById("scoreActuel");
+    let essais = document.getElementById("h1e");
+    essais.innerHTML = comptEE;
+  }
 }
 
 function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
+  firstCard.removeEventListener('click', flipCard); //Met les cartes hors d'état de nuire, les désactive.
   secondCard.removeEventListener('click', flipCard);
-
   resetBoard();
+  
 }
 
 function unflipCards() {
   lockBoard = true;
-
+//Active l'invincibilité des cartes, ce qui ne peux pas nous permettre de les retourner
   setTimeout(() => {
-    firstCard.classList.remove('flip');
+    firstCard.classList.remove('flip'); //Retire la classe qui permet de tourner les cartes
     secondCard.classList.remove('flip');
 
     resetBoard();
-  }, 1500);
+  }, 1500); //Chaque une seconde et demie
 }
 
 function resetBoard() {
@@ -56,8 +76,9 @@ function resetBoard() {
 (function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
+    card.style.order = randomPos;  //permet de mélanger les cartes au lieu de les placer en ordre
   });
 })();
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach(card => card.addEventListener('click', flipCard));  //retourne la carte si tu clique dessus
+
